86342405e26763985242e0956006e27958733302
Adil <adil@ajc-formation.com>
2020-06-14 21:02:48 +0200

    first Operational Version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NewInterfaceRestaurentAmbulant'`, () => {
+  it(`should have as title 'BasicJWTProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NewInterfaceRestaurentAmbulant');
+    expect(app.title).toEqual('BasicJWTProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('NewInterfaceRestaurentAmbulant app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('BasicJWTProject app is running!');
   });
 });
